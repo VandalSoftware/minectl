ccec68a993c335df1a567081cea5355853ac33bd
Vandal <vandal@vandal.io>
2015-10-26 05:05:15 +0000

    Add new subcommands

--- cmd/minectl/main.go
+++ cmd/minectl/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	_ "os"
+	"os"
 )
 
 type latest struct {
@@ -28,10 +29,13 @@ type versions struct {
 const v = "https://s3.amazonaws.com/Minecraft.Download/versions/versions.json"
 
 func main() {
+	flag.Parse()
+
 	// update
 	resp, err := http.Get(v)
 	if err != nil {
 		log.Println("fetch error", err.Error())
+		os.Exit(1)
 		return
 	}
 
@@ -39,6 +43,7 @@ func main() {
 
 	if resp.StatusCode != 200 {
 		log.Println(resp.Status)
+		os.Exit(1)
 		return
 	}
 
@@ -58,8 +63,22 @@ func main() {
 	var versions versions
 	if err := d.Decode(&versions); err != nil {
 		log.Println("couldn't parse %s", fname)
+		os.Exit(1)
 		return
 	}
 
-	fmt.Printf("release: %s, snapshot: %s\n", versions.Latest.Release, versions.Latest.Snapshot)
+	if len(flag.Args()) == 0 {
+		fmt.Printf("release: %s, snapshot: %s\n", versions.Latest.Release, versions.Latest.Snapshot)
+		return
+	}
+
+	switch flag.Args()[0] {
+	case "release":
+		fmt.Println(versions.Latest.Release)
+	case "snapshot":
+		fmt.Println(versions.Latest.Snapshot)
+	default:
+		fmt.Println("minectl: invalid command --", flag.Args()[0])
+		fmt.Println("Try 'minectl --help' for more information")
+	}
 }
